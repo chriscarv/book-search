3e61847c4bf6ef31bae44d7bc33dcd2411c25810
christian <100375218+chriscarv@users.noreply.github.com>
2022-08-21 22:55:17 -0400

    deploy

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 import { Form, Button, Alert } from 'react-bootstrap';
-//import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
-import { useMutation } from "@apollo/client";
-import { ADD_USER } from "../utils/mutations";
 
 const SignupForm = () => {
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+  // define Mutation for ADD_USER
+  const [ addUser, { error }] = useMutation(ADD_USER);
   // set state for form validation
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
-  const [ addUser] = useMutation(ADD_USER);
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
@@ -29,6 +30,7 @@ const SignupForm = () => {
     }
 
     try {
+      // execute addUser mutation and pass in variable data from form
       const { data } = await addUser({
         variables: {...userFormData}
       });
@@ -100,6 +102,7 @@ const SignupForm = () => {
           Submit
         </Button>
       </Form>
+      {error && <div>Sign up failed</div>}
     </>
   );
 };
